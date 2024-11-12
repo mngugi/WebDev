e04ecd644228b188875c864a3477932c9add4ccc
M.Ngugi <mngugi7@gmail.com>
2024-11-12 13:18:23 +0300

    Create addingtwoNumbers.php

---
+++ addingtwoNumbers.php
@@ -0,0 +1,18 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Document</title>
+</head>
+<body>
+    <?php
+        $x = 55;
+        $y = 85;
+        $sum = $x + $y;
+        echo "Total Sum is:", $sum;
+    
+    
+    ?>
+</body>
+</html>
