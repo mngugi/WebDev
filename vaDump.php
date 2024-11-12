d5f72211ce3dd6d9aa423708a78408f8e67fe334
M.Ngugi <mngugi7@gmail.com>
2024-11-12 22:17:09 +0300

    Create vaDump.php

---
+++ vaDump.php
@@ -0,0 +1,14 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Get type</title>
+</head>
+<body>
+    <?php
+    $j = 8;
+    var_dump($j);
+    ?>
+</body>
+</html>
