fbb3c1df6bb964346d8b1c1335791672884a41ff
M.Ngugi <mngugi7@gmail.com>
2024-11-12 22:32:53 +0300

    Update vaDump.php

--- vaDump.php
+++ vaDump.php
@@ -10,12 +10,17 @@
     
     
     var_dump(2);
-    
+    echo("<br>");
     var_dump(0.3);
+    echo("<br>");
     var_dump(3.14);
+    echo("<br>");
     var_dump(true);
+    echo("<br>");
     var_dump([2, 3, 56]);
+    echo("<br>");
     var_dump(NULL);
+    echo("<br>");
     ?>
 </body>
 </html>
