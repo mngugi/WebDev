644252584397256abdf2514b3fad46188a6aff5c
M.Ngugi <mngugi7@gmail.com>
2024-11-12 13:19:44 +0300

    Create multiLine.php

---
+++ multiLine.php
@@ -0,0 +1,15 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Document</title>
+</head>
+<body>
+    <?php
+        echo "Welcome to Php!"; 
+        echo "<br>";
+        echo "It is a server side program.";
+    ?>
+</body>
+</html>
