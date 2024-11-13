5e20d8bfe4e3faf78f933469cc21ea97fd93397f
M.Ngugi <mngugi7@gmail.com>
2024-11-13 13:05:19 +0300

    Create round.php

---
+++ round.php
@@ -0,0 +1,15 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Rounding Function</title>
+</head>
+<body>
+    <?php
+        echo(round(0.7654));
+        echo(round(0.2876));
+    ?>
+    
+</body>
+</html>
