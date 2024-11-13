4c45fc2e462394219013c4de6a63935ded2fe0bf
M.Ngugi <mngugi7@gmail.com>
2024-11-13 13:41:09 +0300

    Create randomPick.php

---
+++ randomPick.php
@@ -0,0 +1,14 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Random Pick</title>
+    <?php
+        echo(rand(10,100));
+    ?>
+</head>
+<body>
+    
+</body>
+</html>
