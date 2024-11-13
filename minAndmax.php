170bc172f1e20feb6f793ad14188d8c06e9cf8e4
M.Ngugi <mngugi7@gmail.com>
2024-11-13 12:57:35 +0300

    Update minAndmax.php

--- minAndmax.php
+++ minAndmax.php
@@ -6,6 +6,11 @@
     <title>Min and Max Function</title>
 </head>
 <body>
-    
+    <?php
+        echo(min(0,123,30,340,146,-6,-90,398));
+        echo(max(0,123,30,340,146,-6,-90,398));
+
+
+    ?>
 </body>
 </html>
